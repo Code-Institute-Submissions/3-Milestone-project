f082b3d3e89f20f6a2b03219e06aacbda7be9286
unknown <diebraga0055@gmail.com>
2020-08-11 09:09:24 +0100

    reccomended python indentation

--- backend/app.py
+++ backend/app.py
@@ -21,58 +21,58 @@ db = mongo.db.users
 @app.route('/users', methods=["POST"])
 # Func is gonna create user and return id
 def createUser():
-  id = db.insert({
-    "name": request.json['name'],
-    "email": request.json['email'],
-    "password": request.json['password']
-  })
-  return jsonify(str(ObjectId(id)))
+    id = db.insert({
+      "name": request.json['name'],
+      "email": request.json['email'],
+      "password": request.json['password']
+    })
+    return jsonify(str(ObjectId(id)))
 
 
 @app.route('/users', methods=['GET'])
 def getUsers():
 # Func is gonna list all users in the database
-  users = []
-  for doc in db.find():
-    users.append({
-      '_id': str(ObjectId(doc['_id'])),
-      'name': doc['name'],
-      'email': doc['email'],
-    })
-  return jsonify(users)
+    users = []
+    for doc in db.find():
+      users.append({
+        '_id': str(ObjectId(doc['_id'])),
+        'name': doc['name'],
+        'email': doc['email'],
+      })
+    return jsonify(users)
 
 
 @app.route('/user/<id>', methods=["GET"])
 def getUser(id):
 # Func is gonna list user by _id
-  user = db.find_one({'_id': ObjectId(id)})
-  return jsonify({
-    '_id': str(ObjectId(user['_id'])),
-    'name': user['name'],
-    'email': user['email'],
-  })
+    user = db.find_one({'_id': ObjectId(id)})
+    return jsonify({
+      '_id': str(ObjectId(user['_id'])),
+      'name': user['name'],
+      'email': user['email'],
+    })
 
 
 @app.route('/users/<id>', methods=['DELETE'])
 def deleteUser(id):
 # Func is gonna delete user by _id
-  db.delete_one({'_id': ObjectId(id)})
-  return jsonify({
-    'message': 'User has been deleted'
-  })
+    db.delete_one({'_id': ObjectId(id)})
+    return jsonify({
+      'message': 'User has been deleted'
+    })
 
 
 @app.route('/users/<id>', methods=['PUT'])
 def updateUser(id):
 # Func is gonna update user by _id
-  db.update_one({'_id': ObjectId(id)}, {"$set": {
-    'name': request.json['name'],
-    'email': request.json['email'],
-    'password': request.json['password']
-  }})
-  return jsonify({
-    'message': 'User has been updated'
-  })
+    db.update_one({'_id': ObjectId(id)}, {"$set": {
+      'name': request.json['name'],
+      'email': request.json['email'],
+      'password': request.json['password']
+    }})
+    return jsonify({
+      'message': 'User has been updated'
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
